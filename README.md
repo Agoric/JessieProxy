f2062f59d3b2b75b39d43d72c9536208ac845fc8
Mark S. Miller <erights@gmail.com>
2019-03-07 17:14:18 -0800

    initial commit of JessieProxy

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JessieProxy
 A subset of the Proxy API as applicable to a standalone Jessie implementation
+
+We assume a standalone Jessie implementation supports only non-mutable
+properties and non-extensible objects, as well as only a very weak
+notion of inheritance. `JessieProxy` is intended to act like the
+subset of `Proxy` applicable only to such constrained objects.
+
---
+++ src/JessieProxy.js
@@ -0,0 +1,54 @@
+/*global harden getOwnPropertyDescriptors create getPrototypeOf makeSet*/
+
+const goodProtos = makeSet([
+  null, Object.prototype, Array.prototype, Function.prototype
+]);
+
+function revocable(target, handler) {
+
+  const descs = getOwnPropertyDescriptors(target);
+
+  const newDescs = {};
+
+  for (const [name, desc] of Object.entries(descs)) {
+    if (desc.configurable !== false) {
+      throw new TypeError(
+        `Standalone Jessie assumes all properties non-configurable: ${name}`);
+    }
+    if (desc.writable === true) {
+      throw new TypeError(
+        `Standalone Jessue assumes all data properties non-writable: ${name}`);
+    }
+    
+    newDescs[name] = harden({
+      get() { return handler.get(target, name, proxy); },
+      set(newVal) { handler.set(target, name, newVal, proxy); },
+      enumerable: desc.enumerable,
+      configurable: false
+    });
+  }
+
+  const proto = getPrototypeOf(target);
+  if (!(goodProtos.has(proto))) {
+    throw new TypeError(
+      `Standablone Jessie assumes direct objects, arrays, or functions`);
+  }
+
+  const proxy = create(proto, newDescs);
+  
+  return harden({
+    proxy,
+    revoke: () => {target = null; handler = null;}
+  });
+}
+
+function makeProxy(target, handler) {
+  return revocable(target, handler).proxy;
+}
+
+const JessieProxy = {
+  revocable,
+  makeProxy
+};
+
+export default JessieProxy;
